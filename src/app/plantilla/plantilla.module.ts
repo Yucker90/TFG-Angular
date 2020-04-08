b2074e828fd4a64d4d97d7a4a318b7fb04b668a4
Yucker <yucker14n@gmail.com>
2020-04-08 23:31:51 +0200

    layout ok, event list TODO

--- src/app/plantilla/plantilla.module.ts
+++ src/app/plantilla/plantilla.module.ts
@@ -4,12 +4,13 @@ import { ShellComponent } from './shell/shell.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
 import { FooterComponent } from './shell/footer/footer.component';
-import { SidebarComponent } from './shell/sidebar/sidebar.component';
+import { NavbarComponent } from './shell/navbar/navbar.component';
+import { EventListComponent } from './shell/event-list/event-list.component';
 
 
 
 @NgModule({
-  declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent, SidebarComponent],
+  declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent, NavbarComponent, EventListComponent],
   imports: [
     CommonModule
   ],
--- src/app/plantilla/shell/event-list/event-list.component.spec.ts
+++ src/app/plantilla/shell/event-list/event-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidebarComponent } from './sidebar.component';
+import { EventListComponent } from './event-list.component';
 
-describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+describe('EventListComponent', () => {
+  let component: EventListComponent;
+  let fixture: ComponentFixture<EventListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [ EventListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
+    fixture = TestBed.createComponent(EventListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
